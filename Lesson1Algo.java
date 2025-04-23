169c9c661f72d05640e80e492cc2ff64e30a0c8d
Chris-Laura-H <chris.laura.hinschberger@gmail.com>
2025-04-23 11:49:24 +0200

    Lesson

---
+++ Lesson1Algo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+ 
+    /***--------Donner un nom à votre class -------------------****/
+ 
+public class Lesson1Algo {
+ 
+ 
+    public static void main(String[] args) {
+ 
+          /***--------  Début du code  -------------------****/
+ 
+ 
+          int a;
+ 
+ 
+          Scanner sc = new Scanner(System.in);
+ 
+          a = sc.nextInt();
+ 
+          System.out.println(a);
+ 
+          sc.close();
+ 
+ 
+        /***--------  Fin du code  -------------------****/
+ 
+    }
+ 
+}
