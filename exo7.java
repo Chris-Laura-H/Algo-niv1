092d6a744151db2b360368df0d581b972f6b60df
Chris-Laura-H <chris.laura.hinschberger@gmail.com>
2025-04-24 09:51:31 +0200

    exo7

--- exo7.java
+++ exo7.java
@@ -15,3 +15,35 @@
  * 
  * 
 */
+import java.util.Scanner; /* Importation de la bibliothèque Java Scanner */
+
+public class exo7 { /* Nom de la Classe */
+
+
+
+    public static void main(String[] args) { /* Toujours le mettre pour tester votre code */
+
+        /***--------  Début du code  -------------------****/
+
+
+        String english; //  Déclaration d'une variable a de Type Entier */
+        
+        
+          Scanner clavier = new Scanner(System.in); // Déclaration du scanner Instance de l'Objet Scanner 
+
+          System.out.println("Do you speak english (Y/N)");
+
+          english = clavier.nextLine(); // Affection de la valeur Scanner de type Text tapé sur le clavier à la variable prenom
+
+          System.out.println("Votre réponse :"+english);
+
+          if (english.equals("Y")) {
+            System.out.println("Nice to meet you");
+        } else {
+            System.out.println("So learn english !");
+        }
+ 
+        clavier.close(); // Ferme le scanner
+   
+    }
+}
