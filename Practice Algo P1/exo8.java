2a8f7cd76d36431a1cdd9884e269a58a7ca94858
Chris-Laura-H <chris.laura.hinschberger@gmail.com>
2025-04-29 11:18:21 +0200

    petitdejeunerexo8

--- Practice Algo P1/exo8.java
+++ Practice Algo P1/exo8.java
@@ -27,3 +27,36 @@
  * Merci Eudes KONDA , votre inscription a bien été effectuée, vous recevrez un mail de confirmation à l'adresse : ekonda@arfp.asso.fr
  * 
 */
+import java.util.Scanner; // Importe le scanner par java
+ 
+    public class exo8 {
+ 
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in); // Déclaration du scanner
+ 
+        System.out.println("Votre prénom ?");
+        String prenom = sc.nextLine();
+
+        System.out.println("Votre nom ?");
+        String nom = sc.nextLine();
+
+        System.out.println("Votre adresse ?");
+        String adresse = sc.nextLine();
+
+        System.out.println("votre mot de passe ?");
+        String mdp = sc.nextLine();
+
+        System.out.println("confirmer votre mot de passe ?");
+        String mdp2 = sc.nextLine();
+ 
+        if(mdp.equals(mdp2))
+            System.out.println("Merci" +prenom+ nom+ ", votre inscription a bien été effectuée, vous recevrez un mail de confirmation à l'adresse :" +adresse);
+
+        else 
+            System.out.println("inscription annulée");
+
+        sc.close(); // Ferme le scanner
+       
+    }
+}
