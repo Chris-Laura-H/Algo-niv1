17178708e0f935ddfae3178902acb2f96b45193c
Chris-Laura-H <chris.laura.hinschberger@gmail.com>
2025-04-30 09:55:26 +0200

    exo10

--- Practice Algo P1/exo10.java
+++ Practice Algo P1/exo10.java
@@ -9,3 +9,65 @@
  * 
  * 
 */
+import java.util.Scanner; // Importe le scanner par java
+ 
+    public class exo10 {
+ 
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+
+        int score = 0;
+
+        System.out.println("Est que pile est pile ?");
+        String reponse1 = sc.nextLine();
+
+        if (reponse1.equals("y")){
+            score= score +1;
+            System.out.println("Bonne réponse, vous avez un point.");
+        }
+        else {System.out.println("Mauvaise réponse");}
+
+        System.out.println("Est que face est face ?");
+        String reponse2 = sc.nextLine();
+
+        if (reponse2.equals("y")){
+            score++;
+            System.out.println("Bonne réponse, vous avez un point.");
+        }
+        else {System.out.println("Mauvaise réponse");}
+
+        System.out.println("Est que a est a ?");
+        String reponse3 = sc.nextLine();
+
+        if (reponse3.equals("y")){
+            score++;
+            System.out.println("Bonne réponse, vous avez un point.");
+        }
+        else {System.out.println("Mauvaise réponse");}
+
+        System.out.println("Est que b est b ?");
+        String reponse4 = sc.nextLine();
+
+        if (reponse4.equals("y")){
+            score++;
+            System.out.println("Bonne réponse, vous avez un point.");
+        }
+        else {System.out.println("Mauvaise réponse");}
+
+        System.out.println("Est que c est c ?");
+        String reponse5 = sc.nextLine();
+        
+        if (reponse5.equals("y")){
+            score++;
+            System.out.println("Bonne réponse, vous avez un point.");
+        }
+        else {System.out.println("Mauvaise réponse");}
+
+        System.out.println("Vous avez " +score+ " points.");
+
+
+        sc.close(); // Ferme le scanner
+       
+    }
+}   
