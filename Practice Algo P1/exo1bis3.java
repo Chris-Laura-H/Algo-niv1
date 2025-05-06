9094be5a6331da28a0dbfd9c0b67d2595aad7a1a
Chris-Laura-H <chris.laura.hinschberger@gmail.com>
2025-05-06 14:05:19 +0200

    xo13corr

---
+++ Practice Algo P1/exo1bis3.java
@@ -0,0 +1,32 @@
+/* EXO1 : A l'aide de Scanner et Print, me faire afficher le résultat d'une addition de deux entiers        
+ * 
+ * 
+* **********************AFFICHAGE ATTENDU ****************:
+ * 
+ * a = 2
+ * b = 3
+ * 
+ * resultat = a + b
+ * 
+ * resulat vaut 5
+ * 
+ * 
+ * 
+*/
+
+import java.util.Scanner;
+
+public class exo1bis3 {
+    public static void main(String[] args) {
+
+    Scanner clavier = new Scanner(System.in);
+    
+    System.out.println("Donner une valeur a");
+    int a = clavier.nextInt();
+
+    System.out.println("Donner une valeur b");
+    int b;
+
+
+}
+}
